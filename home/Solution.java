f2164bab0a2bde7e808d6e00001454566453ff95
DmitriySilin <Demonishishe@gmail.com>
2021-01-15 23:47:49 +0300

    chat

--- home/Solution.java
+++ home/Solution.java
@@ -5,6 +5,7 @@ import ru.ifmo.base.Lesson15.User;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 //Задача №1
 // Вам дан список ролей и сценарий пьесы в виде массива строчек.
@@ -44,10 +45,40 @@ public class Solution {
         System.out.println(sortW);
         return sortW;
     }
+    private static String cod(String text){
+        char[] charText = text.toCharArray();
+        String codText;
+        String finText = "";
+        Map<Character,Integer> characterMap = new TreeMap<>();
+        for (int i = 0; i <charText.length ; i++) {
+            if (characterMap.containsKey(charText[i])){
+                characterMap.replace(charText[i],characterMap.get(charText[i]),characterMap.get(charText[i])+1);
+                if (i == charText.length-1){
+                    finText = finText.concat(String.valueOf(charText[i])).concat(String.valueOf(characterMap.get(charText[i])));
+
+                }
+            }
+            else if (i==0){
+                characterMap.put(charText[i],1);
+            }
+            else{
+                for(Map.Entry<Character,Integer>entry:characterMap.entrySet()){
+                    codText = entry.getKey().toString();
+                    codText = codText.concat(entry.getValue().toString());
+                    finText = finText.concat(codText);
+                }
+                characterMap.clear();
+                characterMap.put(charText[i],1);}
+
+        }
+        return finText;
+    }
 
     public static void main(String[] args) {
         String[] stArr = new String[]{"Роль1: текст1", "Роль2: текст1", "Роль3: текст1", "Роль1: текст2", "Роль2: текст2",
                 "Роль3: текст2", "Роль1: текст3", "Роль2: текст3", "Роль1: текст4", "Роль2: текст4"};
+        String str = "aabbbccccacffff";
 text(stArr);
+System.out.println(cod(str));
     }
 }
